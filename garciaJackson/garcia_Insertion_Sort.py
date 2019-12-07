184cbe58c5c0875184c63d6ad31832759da432df
Jackson <41491633+Jay23Cee@users.noreply.github.com>
2019-12-06 22:01:10 -0800

    Update garcia_Insertion_Sort.py

--- garciaJackson/garcia_Insertion_Sort.py
+++ garciaJackson/garcia_Insertion_Sort.py
@@ -7,18 +7,19 @@ def insertion_sort(List):
     #Getting element from list.
     curNum = List[i]
 
-
+    #starts at 0, then increments by -1,to end at -1 
     for j in range(i-1, -1,  -1 ):
       
       #If Comparing number is greater than The Current Number it takes The Current Number Location.
       if List[j] > curNum:
         List[j+1] = List[j]
+      
       else:
       
       #Else increment J plus 1. and Break out of the for j loop.
         j +=1
         
-        
+        #break out of loop
         break
       #Current number takes J location.
       List[j] = curNum
